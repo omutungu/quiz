c1617bb7782e2d74c1d1e83c8e57f637a458a485
Onesmus <omutungu@gmail.com>
2018-08-19 03:18:20 +0300

    adding logic and operators

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ $(document).ready(function(){
   $(".start").click(function(){
     $("#test").fadeToggle("slow");
   });
+
+  $("form#test").submit(function(event){
+    var Q1 = parseInt($("input:radio[name=Q1]:checked").val());
+    var Q2 = parseInt($("input:radio[name=Q2]:checked").val());
+    var Q3 = parseInt($("input:radio[name=Q2]:checked").val());
+
+    var result = testResults(Q1, Q2 ,Q3);
+
+    $("#display").text("Your score is:" + result);
+
+    event.preventDefault();
+    $(".final").show();
+  });
+});
